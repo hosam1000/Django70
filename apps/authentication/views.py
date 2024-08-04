0d1641d757ed90f41ded6e3ec00a80b070ca779c
Patrick <patrickbasem2000@gmail.com>
2024-08-04 10:18:19 +0300

    84_1018

--- apps/authentication/views.py
+++ apps/authentication/views.py
@@ -1,26 +1,38 @@
+# apps/authentication/views.py
+
+from django.shortcuts import redirect
 from django.views.generic import TemplateView
+from django.contrib.auth import login
 from web_project import TemplateLayout
 from web_project.template_helpers.theme import TemplateHelper
-
-
-"""
-This file is a view controller for multiple pages as a module.
-Here you can override the page view layout.
-Refer to auth/urls.py file for more pages.
-"""
+from .forms import UserRegistrationForm
 
 
 class AuthView(TemplateView):
-    # Predefined function
     def get_context_data(self, **kwargs):
-        # A function to init the global layout. It is defined in web_project/__init__.py file
         context = TemplateLayout.init(self, super().get_context_data(**kwargs))
-
-        # Update the context
         context.update(
             {
                 "layout_path": TemplateHelper.set_layout("layout_blank.html", context),
             }
         )
-
         return context
+
+    def post(self, request, *args, **kwargs):
+        if 'auth_register_basic' in request.path:
+            form = UserRegistrationForm(request.POST)
+            if form.is_valid():
+                user = form.save(commit=False)
+                user.set_password(form.cleaned_data['password'])
+                user.save()
+                login(request, user)
+                return redirect('index')  # Redirect to a success page
+            else:
+                return self.render_to_response(self.get_context_data(form=form))
+        return super().post(request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        if 'auth_register_basic' in request.path:
+            form = UserRegistrationForm()
+            return self.render_to_response(self.get_context_data(form=form))
+        return super().get(request, *args, **kwargs)
